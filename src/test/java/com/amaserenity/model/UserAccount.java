17b366507708deca68ad4eb974e6a2c217d7d63e
Sampad Rout <sampad.rout@gmail.com>
2018-03-15 17:47:41 +0530

    UserAccount data model added

---
+++ src/test/java/com/amaserenity/model/UserAccount.java
@@ -0,0 +1,38 @@
+package com.amaserenity.model;
+
+public class UserAccount {
+    
+	private String username;
+    private String password;
+    private String phonenumber;
+
+//    public UserAccount (String username, String password, String phonenumber) {
+//        this.username = username;
+//        this.password = password;
+//        this.phonenumber = phonenumber;
+//    }
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+}
